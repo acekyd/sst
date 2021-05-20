e9a1f1b59ae568dc9e79b146e2abc5b7c8e7723f
Adewale Abati <adewale@flutterwavego.com>
2021-05-20 17:44:22 +0100

    Base data structure formatting

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,27 @@
       <h1 class="title">
         simple-spot-tracker
       </h1>
-      <p v-if="$strapi.user">
+      <div v-if="$strapi.user">
         Logged in
-      </p>
+        <form @submit="importBinance">
+          <input type="file" required class="form__input" accept=".xlsx, .csv" name="importBinance" id="importBinance" />
+          <input type="submit" value="Import trades"/>
+        </form>
+        <table>
+          <tr>
+            <th>Market</th>
+            <th>Amount</th>
+            <th>Principal</th>
+            <th>No of Trades.</th>
+          </tr>
+          <tbody>
+            <tr v-for="(item, index) in tradeMarketsData" :key="index">
+              <td>{{ index }}</td>
+              <td>{{ item.length }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <div v-else class="authentication__forms">
         <div v-show="error" class="form__errors">
           <p> Error(s): {{ error }} </p>
@@ -54,6 +72,8 @@
 </template>
 
 <script>
+import XLSX from 'xlsx';
+
 export default {
   data() {
     return {
@@ -64,6 +84,9 @@ export default {
       error: "",
       loggedIn: false,
       trades: [],
+      importedData: [],
+      tradeMarketsData: {},
+      tabularData: {},
     };
   },
 
@@ -74,7 +97,7 @@ export default {
   },
 
   computed: {
-    user () {
+    user() {
       console.log(this.$strapi.user);
       return this.$strapi.user;
     },
@@ -133,9 +156,100 @@ export default {
     async fetchTrades() {
       if(this.user) {
         const trades = await this.$strapi.$trades.find({'users_permissions_user.username': this.user.username });
-        console.log("Trades o", trades);
         this.trades = trades;
+        this.processTradesData();
       } else return [];
+    },
+
+    processTradesData() {
+      // this.$set(this.tradeMarketsData, 'trade', [1]);
+      this.trades.forEach(trade => {
+        if(this.tradeMarketsData[trade.market] === undefined) {
+          this.$set(this.tradeMarketsData, trade.market, []);
+        }
+        this.tradeMarketsData[trade.market].push(trade);
+      });
+
+
+
+      for (const [key, trades] of Object.entries(this.tradeMarketsData)) {
+        let title = key;
+        let amount = 0,
+        principal = 0,
+        totalBoughtCost = 0,
+        totalSoldCost = 0,
+        noOfTrades = trades.length;
+        trades.forEach(trade => {
+          console.log(title, trade);
+          if(trade.type === "BUY") {
+            amount += parseFloat(trade.amount);
+            principal += parseFloat(trade.total);
+            totalBoughtCost += parseFloat(trade.total);
+          }
+          else {
+            amount -= parseFloat(trade.amount);
+            principal -= parseFloat(trade.total);
+            totalSoldCost += parseFloat(trade.total);
+          }
+        })
+
+        this.$set(this.tabularData, title, {
+          amount:amount,
+          principal,
+          totalBoughtCost,
+          totalSoldCost,
+          noOfTrades,
+        });
+      }
+    },
+
+    async importBinance(e) {
+      e.preventDefault();
+      let file = document.getElementById("importBinance").files[0];
+      var reader = new FileReader();
+      const self = this;
+			reader.onload = function (e) {
+				// pre-process data
+				var binary = "";
+				var bytes = new Uint8Array(e.target.result);
+				var length = bytes.byteLength;
+				for (var i = 0; i < length; i++) {
+					binary += String.fromCharCode(bytes[i]);
+				}
+
+				/* read workbook */
+				var wb = XLSX.read(binary, {type: 'binary'});
+
+				/* grab first sheet */
+				var wsname = wb.SheetNames[0];
+				var ws = wb.Sheets[wsname];
+
+				/* generate JSON */
+				self.importedData = XLSX.utils.sheet_to_json(ws);
+			};
+      await reader.readAsArrayBuffer(file);
+
+
+      reader.onloadend = function() {
+        self.uploadData();
+      }
+    },
+
+    uploadData() {
+      console.log("I have been called");
+      // Upload each entry to DB
+      this.importedData.forEach(trade => {
+        this.$strapi.$trades.create({
+          amount: trade.Amount,
+          type: trade.Type,
+          total: trade.Total,
+          price: trade.Price,
+          market: trade.Market,
+          users_permissions_user: this.user,
+        });
+      });
+
+      this.fetchTrades();
     }
 
   }
